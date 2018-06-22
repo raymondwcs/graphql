3ab3843cf6556d924bd14c2388e7d9649add3926
Raymond S) <rso@ouhk.edu.hk>
2018-06-22 09:38:40 +0800

    an integrated example

---
+++ demo02/server.js
@@ -0,0 +1,53 @@
+/*
+curl -X POST -H "Content-Type: application/json" --data '{ "query": "{allPersons {name}}"}' http://localhost:4000/graphql
+*/
+
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    invoiceAmtHKD(invoiceNo: String!): invoiceAmt
+  },
+
+  type invoiceAmt {
+    invoiceNo: String!
+    amount: Int!
+  },
+`);
+
+// some hardcoded data
+var invoices = [
+  {invoiceNo: "00001", amt: 70,   currency: "USD"},
+  {invoiceNo: "00002", amt: 1800, currency: "JPY"},
+];
+
+// The root provides a resolver function for each API endpoint
+/*
+var root = {
+  allPersons: () => {
+    return users;
+  },
+};
+*/
+var root = {
+  invoiceAmtHKD: function (args) {
+    var invoiceAmt = {};
+    invoiceAmt['invoiceNo'] = "xxxxx";
+    invoiceAmt['amount'] = 0;
+    return(invoiceAmt);
+  }
+};
+
+
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
