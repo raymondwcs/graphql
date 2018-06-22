82e44e9aca92174cb3139fdfe408d62773f7dc5a
Raymond S) <rso@ouhk.edu.hk>
2018-06-22 11:19:35 +0800

    added body.json

--- demo02/server.js
+++ demo02/server.js
@@ -1,5 +1,11 @@
 /*
-curl -X POST -H "Content-Type: application/json" --data '{ "query": "{allPersons {name}}"}' http://localhost:4000/graphql
+curl -X POST -H "Content-Type: application/json" --data @body.json http://localhost:4000/graphql
+
+In body.json:
+
+{
+    "query": "query {invoiceAmtHKD(invoiceNo: \"00001\") {amount}}"
+}
 */
 
 var express = require('express');
