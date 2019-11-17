1c4c9d2c6ad81cab938f1cd64e75eff5f2e34c97
Raymond SO <rso@ouhk.edu.hk>
2019-11-17 21:20:36 +0800

    initial version

---
+++ friendsGraphQL/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// REST resource: users
+const users = [
+  {id: "1", name: "jane", location: "aud", friends: ["2","3"]},
+  {id: "2", name: "peter", location: "jp", friends: ["1"]},
+  {id: "3", name: "mary", location: "cad", friends: ["1"]}
+];
+
+// REST resource: locations
+const locations = [
+  {id: "aud", name: "australia"},
+  {id: "jp", name: "japan"},
+  {id: "cad", name: "canada"}
+];
+
+// Construct a schema, using GraphQL schema language
+
+const schema = buildSchema(`
+  type Query {
+    friends(name: String!): [Friend]
+  },
+
+  type Friend {
+    name: String
+    location: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {  
+  friends: (args) => {
+    // Get ids of user's friend; filter() returns an array
+    let targetUser = users.filter(u => {
+      return u.name == args.name;
+    });
+    let friendsOfTargetUser = [];
+    // find user's friends
+    targetUser[0].friends.forEach(fid => {
+      users.forEach(u => {
+        if (u.id == fid) friendsOfTargetUser.push(u);
+      })   
+    });
+    return(friendsOfTargetUser);
+   }
+};
+
+// Create an express server and a GraphQL endpoint
+const app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
